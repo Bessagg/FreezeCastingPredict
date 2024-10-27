e2ee7e6612d6ce031e6ae79328521e9ef9515d2b
Bessagg <bessaraid@gmail.com>
2024-10-27 13:59:18 -0300

    added mape. using reduced_feats data

--- analysis_model.py
+++ analysis_model.py
@@ -5,14 +5,20 @@ import warnings
 import glob
 import os
 from scipy.stats import kstest
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.metrics import r2_score
+import seaborn as sns
 import statsmodels.api as sm
 from scipy.stats import pearsonr, chi2_contingency
-
+from scipy.stats import chi2_contingency, kruskal
 warnings.filterwarnings("ignore", category=DeprecationWarning)
 warnings.filterwarnings("ignore", category=Warning)
 pd.set_option('display.max_columns', 100)
 pd.set_option('display.max_rows', 40)
 pd.set_option('display.width', 600)
+
+im_path = 'images/model_analysis'
 
 
 def ks_test_group(group_df, column, distribution='norm'):
@@ -28,7 +34,7 @@ target = parser.target
 
 """Fetch dataset"""
 print("Loading Data...")
-selected_feats_name = "all_cols"
+selected_feats_name = "reduced_feats"
 df_train = pd.read_csv(f"data/{selected_feats_name}/train.csv")
 df_test = pd.read_csv(f"data/{selected_feats_name}/test.csv")
 
@@ -48,16 +54,24 @@ results_importances = []
 results_train = pd.DataFrame()
 results_test = pd.DataFrame()
 results_groups = []
-results_ks_test = []
-results_p_values = []
 for pipe in selected_pipes:
     print(pipe.name, pipe.feats_name)
     pipe_ggparent_dir = os.path.basename(os.path.dirname(os.path.dirname(os.path.dirname(pipe_path))))
     train_preds, test_preds = pipe.predict(df_train), pipe.predict(df_test)
     print("Train results")
-    r2_train, mae_train, mse_train = utils.get_regression_metrics(train_preds, df_train[target])
+    r2_train, mae_train, mse_train, mape_train = utils.get_regression_metrics(train_preds, df_train[target])
     print("Test results")
-    r2_test, mae_test, mse_test = utils.get_regression_metrics(test_preds, df_test[target])
+    r2_test, mae_test, mse_test, mape_test = utils.get_regression_metrics(test_preds, df_test[target])
+    print("Test nulls results")
+    df_test_nulls = df_test.copy()
+    for col in df_test_nulls.columns:
+        if col not in ['name_part1', 'name_fluid1', 'vf_total', 'material_group', target]:
+            if col in parser.all_num_cols:
+                df_test_nulls[col] = np.nan
+            else:
+                df_test_nulls[col] = ""
+    test_preds_n = pipe.predict(df_test_nulls)
+    r2_test_n, mae_test_n, mse_test_n, mape_test_n = utils.get_regression_metrics(test_preds_n, df_test[target])
 
     # Add model results
     result_model = {'name': pipe.name, 'r2_train': r2_train, 'mae_train': mae_train, 'mse_train': mse_train,
@@ -72,10 +86,15 @@ for pipe in selected_pipes:
     estimator = pipe[-1]
     if hasattr(estimator, 'feature_importances_'):
         feature_importances = estimator.feature_importances_
+        # normalize
+        feature_importances = feature_importances / feature_importances.sum()
+        feature_names = pipe[0].get_feature_names_out()
+        feature_names = np.array([name.replace('remainder__', '').replace('cat__', '') for name in feature_names])
+
         for i, importance in enumerate(feature_importances):
             results_importances.append({
                 'model': pipe.name,
-                'feature_index': i,
+                'feature_name': feature_names[i],
                 'feature_importance': importance
             })
 
@@ -86,43 +105,75 @@ for pipe in selected_pipes:
         grouped_train = df_train.groupby(groupby_feature)
         category_counts = grouped_train[groupby_feature].value_counts().sort_values(ascending=False)
         # Determine top 5 most frequent categories
-        top5_categories = category_counts[0:5]
+        top_categories = category_counts[0:5]
 
         for category, category_indices in grouped.groups.items():
-            tag_top5 = category in top5_categories
+            # just for tagging top5 if category in most frequent and not filtering
+            tag_top5 = category in top_categories
+            if not tag_top5:
+                continue
             group_df = df_test.loc[category_indices]
             group_y_true = df_test[target][category_indices]
             group_y_pred = preds_test[category_indices]
-            grouped_r2, grouped_mae, grouped_mse = (
+            grouped_r2, grouped_mae, grouped_mse, grouped_mape = (
                 utils.get_regression_metrics(group_y_pred, group_y_true, opt_print=False))
+            group_variance = round(np.var(group_y_pred), 3)
+            group_std = round(np.std(group_y_true), 2)  # true porosity std
             # append results
             results_groups.append({
                 'model': pipe.name,
                 'groupby_feature': groupby_feature,
                 'category': category,
-                'train samples': df_test[groupby_feature].value_counts()[category],
+                'train samples': df_train[groupby_feature].value_counts()[category],
                 'test_samples': df_test[groupby_feature].value_counts()[category],
                 'r2': grouped_r2,
                 'mae': grouped_mae,
                 'mse': grouped_mse,
+
+                'variance': group_variance,
+                'std': group_std,
+                'mape': grouped_mape,
                 'top5': tag_top5,
             })
-
-            # Perform K-S Test
-            ks_result = ks_test_group(group_df, target)
-            ks_result['model'] = pipe.name
-            ks_result['groupby_feature'] = groupby_feature
-            ks_result['category'] = category
-            ks_result['p_value'] = ks_result['p_value']
-            ks_result['top5'] = tag_top5
-            results_ks_test.append(ks_result)
 
     print('\n')
 
 
 """Plots"""
 selected_model = 'catb'
+df_results = pd.DataFrame(results_groups)
+df_imps = pd.DataFrame(results_importances)
 df_groups = pd.DataFrame(results_groups)
-df_ks_tests = pd.DataFrame(results_ks_test)
-print(df_ks_tests[(df_ks_tests['top5'] == True) & (df_ks_tests['model'] == selected_model)].sort_values(by='p_value', ascending=False))
+df_groups_filtered = df_groups.query("model == 'catb' and top5 == True").sort_values(by=['groupby_feature', 'train samples'], ascending=False)
+print("Selected model results by group:")
+print(df_groups_filtered)
 
+
+print("Relationship between R2 and STD")
+df_groups_filtered['variance'] = df_groups_filtered['r2'].astype(float)
+df_groups_filtered['std'] = df_groups_filtered['std'].astype(float)
+plt.scatter(df_groups_filtered['r2'].astype(float), df_groups_filtered['std'].astype(float))
+plt.xlabel(r'$R^2$ value')
+plt.ylabel("STD")
+from sklearn.linear_model import LinearRegression
+X = df_groups_filtered[['std']].values.reshape(-1, 1)  # Independent variable
+# X = X.flatten().astype(float).tolist()
+y = df_groups_filtered['r2'].values  # Dependent variable
+y = y.astype(float)
+# Fit the linear regression model
+model = LinearRegression()
+model.fit(X, y)
+# Predict y values
+y_pred = model.predict(X)
+# Calculate the R^2 score
+r2 = r2_score(y, y_pred)
+# Plot the results
+plt.clf()
+plt.scatter(X, y, color='blue', label='Grouped data')
+plt.plot(X, y_pred, color='red', linewidth=2, label=f'Linear regression (R² = {r2:.2f})')
+plt.xlabel('Standard Deviation of true porosity')
+plt.ylabel(r'$R^2$ Value')
+plt.title('Linear Regression between STD and $R^2$')
+plt.legend()
+plt.show()
+plt.savefig(f"{im_path}/r2_vs_std.png")
